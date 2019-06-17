c7b4e69c09197bc919af21da0b23d90f46454ec0
Tony Huynh <t.hnh@tuta.io>
2019-06-17 11:17:09 +1000

    added edit and delete routes

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -8,11 +8,30 @@ class OrderItemsController < ApplicationController
         @order_item = OrderItem.new
         @order_item.name = params[:order_item][:name]
         @order_item.quantity = params[:order_item][:quantity]
-        
+
         if @order_item.save
             redirect_to order_items_path
         else
             render "new"
         end
+    end
+
+    def edit
+        @order_item = OrderItem.find(params[:id])
+    end
+
+    def update
+        @order_item = OrderItem.find(params[:id])
+        @order_item.quantity = params[:order_item][:quantity]
+        if @order_item.save
+            redirect_to order_items_path
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        OrderItem.destroy(params[:id])
+        redirect_to order_items_path   
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get "/order", to: "orders#index", as: "order_items"
   get "/order/new", to: "order_items#new", as: "new_order_item"
   post "/order", to: "order_items#create"
+  # get "/order_items/:id/edit", to: "order_items#edit", as: "edit_order_item"
+  # put "/order_items/:id", to: "order_items#update"
+  # patch "/order_items/:id", to: "order_items#update", as: "edit_order_item"
+  resources :order_items, only: %i[edit update destroy]
 end
